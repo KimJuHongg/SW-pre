8b3ca62babc2d7b733465c7bfa2457554856a202
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-18 12:03:01 +0900

    Update ex5.java

--- 230718/ex5.java
+++ 230718/ex5.java
@@ -1,8 +1,10 @@
 //영수증에는 10% 부가세와 잔돈이 인쇄되어 있다. 구입한 상품의 가격과 받은 금액을 입력하면 부가세와 잔돈을 출력하는 프로그램을 작성하시오.
 
+package day0718;
+
 import java.util.Scanner;
 
-public class text5 {
+public class ex5 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
