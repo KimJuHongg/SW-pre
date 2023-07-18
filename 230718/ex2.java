a66fe469ecab7f18b55c336f7cebe7c48e372d45
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-18 15:15:41 +0900

    Create ex2.java

---
+++ 230718/ex2.java
@@ -0,0 +1,35 @@
+//단어 하나 입력해 모음, 자음 개수 세기
+
+package day0718;
+
+import java.util.Scanner;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("단어를 입력하시오 : ");
+		String word = scan.nextLine();
+		
+		
+		int consonants_num = 0;
+		int vowels_num = 0;
+		
+		for(int i = 0; i < word.length(); i++) {
+			if(word.charAt(i) == 'a' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'e' || word.charAt(i) == 'o') {
+				
+				vowels_num += 1;
+			}
+			else { 
+				
+				consonants_num += 1;
+			}
+		
+		}
+		
+		System.out.println("모음개수 : " + vowels_num + ", 자음 개수 : " + consonants_num);
+		scan.close();
+	}
+
+}
