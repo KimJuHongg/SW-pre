1db9cf1bbe4916264c207f1dd6d2edde0b653594
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-18 15:40:01 +0900

    Update ex2.java

--- 230718/ex2.java
+++ 230718/ex2.java
@@ -4,7 +4,7 @@ package day0718;
 
 import java.util.Scanner;
 
-public class ex2 {
+/*public class ex2 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -16,20 +16,44 @@ public class ex2 {
 		int consonants_num = 0;
 		int vowels_num = 0;
 		
-		for(int i = 0; i < word.length(); i++) {
-			if(word.charAt(i) == 'a' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'e' || word.charAt(i) == 'o' ||word.charAt(i) == 'A' || word.charAt(i) == 'U' || word.charAt(i) == 'I' || word.charAt(i) == 'E' || word.charAt(i) == 'O') {
-				
-				vowels_num += 1;
-			}
-			else { 
-				
-				consonants_num += 1;
-			}
 		
-		}
-		
+		  for(int i = 0; i < word.length(); i++) { 
+			  if(word.charAt(i) == 'a' || word.charAt(i) == 'u' || word.charAt(i) == 'i' || word.charAt(i) == 'e' || word.charAt(i) == 'o') {
+		  
+				  vowels_num += 1; 
+			  
+			  } else {
+		  
+				  consonants_num += 1; 
+				  }
+		  }
+	
 		System.out.println("모음개수 : " + vowels_num + ", 자음 개수 : " + consonants_num);
 		scan.close();
+	}
+
+}*/
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("단어를 입력하시오 : ");
+		String str = scan.nextLine();
+		
+		int cntA = 0;
+		int cntB = 0;
+		for(int i =0; i< str.length(); i++) {
+			switch(str.charAt(i)) {
+			case 'a','e','o','u','i', 'A', 'E', 'O', 'U', 'I' -> cntA++;
+			default -> cntB++;
+			}
+		}
+		System.out.println("모음개수 : " + cntA + ", 자음 개수 : " + cntB);
+		scan.close();
+		
+		
 	}
 
 }
