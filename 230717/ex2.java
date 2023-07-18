02c94c174aa2f8136ab3b3ff1d038eed1006c651
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-18 11:17:13 +0900

    Update ex2.java

--- 230717/ex2.java
+++ 230717/ex2.java
@@ -1,17 +1,18 @@
 //２．　마일을　킬로미터로　변환하는　프로그램을　작성하시오．입력을　받아서　변환함．　ｎｅｘｔＤｏｕｂｌｅ（）　이용
+
 import java.util.Scanner;
 
 public class test2 {
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("마일 입력 : ");
-		Double a = scanner.nextDouble();
+		Double a = scan.nextDouble();
 		double b = a * 1.60934;
 		
 		System.out.println(b);
-		
+		scan.close();
 		
 	}
 
