3703724f04d7b19569400612159f60f255fe104a
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-18 16:09:27 +0900

    Create p133.java

---
+++ Exercise/p133.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class P133 {
+
+	public static void main(String[] args) {
+		System.out.println("메시지를 입력하세요");
+		System.out.println("프로그램을 종료하려면 q를 입력하세요.");
+		
+
+		Scanner scan = new Scanner(System.in);
+		String inputString;
+		
+		do {
+			System.out.print(">");
+			inputString = scan.nextLine();
+			System.out.println(inputString);
+		} while (!inputString.equals("q") );
+		
+		System.out.println();
+		System.out.println("프로그램 종료");
+	}
+
+}
