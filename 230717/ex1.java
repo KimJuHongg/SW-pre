5d0b10d97d1e1f684cb25b1e321c82ebb7c35da9
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-18 10:18:35 +0900

    Create ex1.java

---
+++ 230717/ex1.java
@@ -0,0 +1,13 @@
+//１．하나의　상자에　오렌지를　１０개씩　담을　수　있다．　오렌지가　１２７개　있다면　상자　몇 개가　필요한가？　또　몇 개의　오렌지가　남을까？
+
+public class test1 {
+	public static void main(String[] args) {
+		
+		int a = 127 / 10;
+		int b = 127 % 10;
+		System.out.println("상자 개수 : "+ a);
+		System.out.println("남은 오렌지 개수 : "+ b);
+	
+	}
+
+}
