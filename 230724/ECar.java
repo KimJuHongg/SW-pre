e9d0797c3e6e43a89f5fe4f5619a374c14e0d18f
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-24 14:55:29 +0900

    Create ECar.java

---
+++ 230724/ECar.java
@@ -0,0 +1,44 @@
+package day0724;
+
+public class ECar {
+
+	int distance = 0;
+	int battery = 100;
+	
+	
+	public int getDistance() {
+		return distance;
+	}
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	public int getBattery() {
+		return battery;
+	}
+	public void setBattery(int battery) {
+		this.battery = battery;
+	}
+	
+	public void dispDistance() {
+		System.out.println("총주행거리 : "+ distance + "km");
+	}
+	
+	public void dispBattery() {
+		System.out.println("배터리충전량 : "+ battery + "%");
+	}
+	
+	public void charge() {
+		battery -= 10;
+	}
+	
+	public void full_battery() {
+		battery = 100;
+	}
+	
+	public void drive() {
+		distance++;
+	}
+	
+	
+	
+}
