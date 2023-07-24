2ab1b28b1b99615b4f8d4beff914f981981286c9
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-24 13:57:07 +0900

    Create Rect.java

---
+++ Exercise/Rect.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class Rect {
+
+	int width, height;
+	
+	public Rect(int w, int h) {
+		this.width = w;
+		this.height = h;
+	}
+	
+	double calcArea() {
+		return (double)width*height;
+	}
+}
