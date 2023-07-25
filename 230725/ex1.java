ea58030a91bb71c434d0e338e3959f4034f78f39
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-25 14:52:12 +0900

    Create ex1.java

---
+++ 230725/ex1.java
@@ -0,0 +1,15 @@
+package day0725;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(char i = 'a'; i <= 'z'; i++ ) {
+			sb.append(i+ " ");
+			
+		}
+		System.out.print(sb);
+	}
+
+}
