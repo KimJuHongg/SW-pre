50906d4045ef2e49468a4641c6d856b0e7d24bc9
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-26 15:42:52 +0900

    Create MagazineExample.java

---
+++ 230726/MagazineExample.java
@@ -0,0 +1,28 @@
+package day0726;
+
+import java.util.Scanner;
+
+public class MagazineExample {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("잡지 이름은? ");
+		String title = scan.nextLine();
+		
+		System.out.println("페이지수는? ");
+		int pages = scan.nextInt();
+		
+		System.out.print("저자는? ");
+		String writer = scan.nextLine();
+		
+		System.out.println("발매일은? ");
+		String date = scan.nextLine();
+		
+		Magazine magazine = new Magazine(title, pages, writer, date);
+		magazine.printInfo();
+		
+		scan.close();
+		
+	}
+}
