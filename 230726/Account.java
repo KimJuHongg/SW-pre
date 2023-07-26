3edf4e43b752bb1dac43bb681f0e4d4b30fc4f1b
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-26 15:43:04 +0900

    Create Account.java

---
+++ 230726/Account.java
@@ -0,0 +1,44 @@
+//연습문제20번 Account계좌클래스를 배열로 구현
+
+package day0726;
+
+public class Account {
+	
+	String account;
+	String name;
+	int money;
+	
+	public Account(String account, String name, int money) {
+		this.account = account;
+		this.name = name;
+		this.money = money;
+	}
+	
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	@Override
+	public String toString() {
+		return "[ " + account + ", " + name + ", " + money +" ] ";
+	}
+}
