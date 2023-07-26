406265f872da4ccab70fdc1774b666bca30a57f9
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-26 15:41:47 +0900

    Create Parent.java

---
+++ 230726/Parent.java
@@ -0,0 +1,14 @@
+//p308
+package day0726;
+
+public class Parent {
+	//메소드 선언
+	public void method1() {
+		System.out.println("Parent-method1()");
+	}
+	
+	//메소드 선언
+	public void method2() {
+		System.out.println("Parent-method2()");
+	}
+}
