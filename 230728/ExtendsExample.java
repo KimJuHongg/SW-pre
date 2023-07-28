6ade6cc873e524394bc8c0a03a30643056e7757f
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-07-28 15:23:49 +0900

    Create ExtendsExample.java

---
+++ 230728/ExtendsExample.java
@@ -0,0 +1,24 @@
+package day0728;
+
+public class ExtendsExample {
+	public static void main(String[] args) {
+		InterfaceImpl impl = new InterfaceImpl();
+		
+		InterfaceA ia = impl;
+		ia.methodA();
+		//ia.methodB();
+		System.out.println();
+		
+		InterfaceB ib = impl;
+		//ib.methodA();
+		ib.methodB();
+		System.out.println();
+		
+		InterfaceC ic = impl;
+		ic.methodA();
+		ic.methodB();
+		ic.methodC();
+		System.out.println();
+		
+	}
+}
