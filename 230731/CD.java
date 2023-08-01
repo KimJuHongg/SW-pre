2773223a9bb20dbe2cab4d559026ab4b68ab2780
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-08-01 12:06:25 +0900

    Create CD.java

---
+++ 230731/CD.java
@@ -0,0 +1,33 @@
+package day0731;
+
+public class CD
+{
+	private String registerNo;
+	private String CDTitle;
+	
+	public CD(String registerNo, String CDTitle)
+	{
+		this.registerNo = registerNo;
+		this.CDTitle = CDTitle;
+	}
+
+	public String getRegisterNo()
+	{
+		return registerNo;
+	}
+
+	public void setRegisterNo(String registerNo)
+	{
+		this.registerNo = registerNo;
+	}
+
+	public String getCDTitle()
+	{
+		return CDTitle;
+	}
+
+	public void setCDTitle(String CDTitle)
+	{
+		this.CDTitle = CDTitle;
+	}
+}
