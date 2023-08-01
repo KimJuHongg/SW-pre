76655e6bebed62af938f2f0df7355ba939dc4dc9
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-08-01 11:59:53 +0900

    Create HashSetExample2.java

---
+++ 230801/HashSetExample2.java
@@ -0,0 +1,20 @@
+package day0801;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample2 {
+
+	public static void main(String[] args) {
+		//HashSet 컬렉션 생성
+		Set<Member1> set = new HashSet<Member1>();
+		
+		//Member 객체 저장
+		set.add(new Member1("홍길동", 30)); 	//->인스턴스는 다르지만 동등 객체이므로 객체 1개만 저장
+		set.add(new Member1("홍길동", 30));
+		
+		//저장된 객체 수 출력
+		System.out.println("총 객체 수 : "+ set.size());
+	}
+
+}
