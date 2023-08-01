38e1ef1dba9704d5bce2fe201c33a24ff90cdfab
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-08-01 22:02:02 +0900

    Create Person.java

---
+++ 230801/Person.java
@@ -0,0 +1,39 @@
+package 230801;
+
+public class Person implements Comparable<Person> {
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		this.name =name;
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "이름 : " + name + ", 나이 : " + age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age =age;
+	}
+	
+	@Override
+	public int compareTo(Person o) {
+		if(o.name.equals(name) && age == o.age)
+			return 0;
+		return -1;
+	}
+}
