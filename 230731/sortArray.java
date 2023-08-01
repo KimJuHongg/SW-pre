fdbf06e256e82129d15ca470a825eeb28a9f0499
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-08-01 14:20:31 +0900

    Update sortArray.java

--- 230731/sortArray.java
+++ 230731/sortArray.java
@@ -3,7 +3,7 @@ package day0731;
 import java.util.Arrays;
 
 public class sortArray {
-
+/*
 	public static void main(String[] args)
 	{
 		int[] arr = { 1, 5, 2, 6, 3, 7, 4 };
@@ -31,6 +31,7 @@ public class sortArray {
 		for(int i = 0; i < commands.length; i++)
 		{
 			int[] temp = Arrays.copyOfRange(arr, commands[i][0] - 1, commands[i][1]);
+			
 			Arrays.sort(temp);
 			result[i] = temp[commands[i][2] - 1];
 		}
@@ -38,7 +39,42 @@ public class sortArray {
 		return result;
 	}
 }
+*/
+	public static void main(String[] args)
+	{
+		int[] array = { 1, 5, 2, 6, 3, 7, 4 };
+		int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
 
-
-
-
+		int[] result = getResult(array,commands);
+		System.out.print(Arrays.toString(result));
+		
+	}
+	
+	public static int[] getResult(int[] arr, int[][] com) {
+		int[] result = new int[com.length];
+		int[] eachArray;
+		for(int i =0; i<com.length; i++) {
+			eachArray=new int[com[i][1]-com[i][0]+1];
+			for(int j=com[i][0]-1; j<com[i][1]; j++) {
+				eachArray[j-com[i][0]+1] = arr[j];
+			}
+			Arrays.sort(eachArray);
+			result[i] = eachArray[com[i][2]-1];
+		}
+		return result;
+	}
+	/*
+	//copyOfRange()사용
+	public static int[] getResult(int[] arr, int[][] com) {
+		int[] result = new int[com.length];
+		int[] eachArray;
+		for(int i =0; i<com.length; i++) {
+			eachArray=new int[com[i][1]-com[i][0]+1];
+			eachArray=Arrays.copyOfRange(arr, com[i][0]-1,com[i][1]);
+			Arrays.sort(eachArray);
+			result[i]=eachArray[com[i][2]-1];
+		}
+		return result;
+	}
+	*/
+}
