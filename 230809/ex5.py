8ae87ae5f32733607d199a57a9303734853a0ac8
김주홍 <98217140+KimJuHongg@users.noreply.github.com>
2023-08-09 15:34:23 +0900

    Create ex5.py

---
+++ 230809/ex5.py
@@ -0,0 +1,16 @@
+#p204,205
+tt1 = (10, 20, 30); tt1
+tt2 = 10, 20, 30; tt2
+
+print(tt1)
+print(tt2)
+
+tt3 = (10); tt3
+tt4 = 10; tt4
+tt5 = (10,); tt5
+tt6 = 10,; tt6
+
+print(tt3)
+print(tt4)
+print(tt5)
+print(tt6)
